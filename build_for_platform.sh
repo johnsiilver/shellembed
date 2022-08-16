bda6b7980bc89802b82347c2882d9ce20ae2e75c
John Doak <johnsiilver@gmail.com>
2022-08-15 23:41:13 -0700

    Update build_for_platform.sh

--- build_for_platform.sh
+++ build_for_platform.sh
@@ -3,6 +3,15 @@
 export GOOS=linux 
 export GOARCH=amd64 
 go build -o bin/hello_linux_amd64
+
+export GOOS=linux 
+export GOARCH=arm64 
+go build -o bin/hello_linux_arm64
+
+export GOOS=darwin
+export GOARCH=amd64 
+go build -o bin/hello_darwin_amd64
+
 export GOOS=darwin
 export GOARCH=arm64 
 go build -o bin/hello_darwin_arm64
